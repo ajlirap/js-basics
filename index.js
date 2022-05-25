1c5fde56990ec111d8defbe9f26b5f1527625abc
Arturo Lira-Perez <alira@snrha.local>
2022-05-25 11:45:05 -0700

    Restart basic training

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Comment
-console.log('Hello World');
+console.log("Hello World");
 
 //Best Practices you have to declare every variable in different line
-let nameOne = 'Arturo';
-let lastNameOne ='Lira';
-console.log(nameOne +' ' +lastNameOne );
+let nameOne = "Arturo";
+let lastNameOne = "Lira";
+console.log(nameOne + " " + lastNameOne);
 
-let firstName ='ArturoL';
+let firstName = "ArturoL";
 console.log(firstName);
 
 const interestRate = 0.3;
@@ -22,13 +22,13 @@ console.log(interestRate);
  * null
  */
 
-let name = 'Arturo'; // String Literal 
+let name = "Arturo"; // String Literal
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
 //let firstNames = undefined; // It is not necessary to define undefined variable
 let lastNames = null;
 let selectedColor = null;
-selectedColor = 'red';
+selectedColor = "red";
 
 /******VALUE TYPES ******/
 /**
@@ -37,21 +37,21 @@ selectedColor = 'red';
  * Function
  */
 /**OBJECT */
-let person = {name: 'Arturo', age: 38}; //Object Literal
+let person = { name: "Arturo", age: 38 }; //Object Literal
 
 //Dot Notation
-person.name = 'Jose';
+person.name = "Jose";
 //Bracket Notation
-let selection = 'name';
+let selection = "name";
 //person['name'] = 'Juan';
-person[selection] ='Josh';
+person[selection] = "Josh";
 
 console.log(person);
 
 /**ARRAY */
 
-let selectedColors = ['red','blue'];
-selectedColors[2]= 'green';
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "green";
 selectedColors[3] = 3;
 console.log(selectedColors);
 console.log(selectedColors[0]);
@@ -60,19 +60,18 @@ console.log(selectedColors.length);
 /**FUNCTION */
 
 /**Performing a task */
-function greet(name,lastName) {
-    console.log('Hello World inside of function declaration');
-    console.log('Hello ' + name +' '+ lastName);
+function greet(name, lastName) {
+  console.log("Hello World inside of function declaration");
+  console.log("Hello " + name + " " + lastName);
 }
-greet('Arturo', 'Lira');
+greet("Arturo", "Lira");
 
 /**Calculating a value */
-function square(number)
-{
-    return number*number;
+function square(number) {
+  return number * number;
 }
 
-let number= square(2);
+let number = square(2);
 console.log(square(2));
 
 /**OPERATOR **/
@@ -85,8 +84,8 @@ console.log(square(2));
  */
 /**Arithmetic */
 
-let x=10;
-let y=3;
+let x = 10;
+let y = 3;
 
 // console.log(x + y);  /**addition */
 // console.log(x - y); /**Subtraction */
@@ -105,9 +104,7 @@ console.log(x--);
 
 /**Assignment operator */
 
-
 /**Exercises  */
-
 
 /**FizzBuzz*/
 
@@ -117,24 +114,19 @@ console.log(x--);
 //Not divisible by 3 or 5 ==> input
 //Not a number ==> 'Not a number'
 
-const output = fizzBuzz('valuea');
+const output = fizzBuzz("valuea");
 console.log(output);
 
 function fizzBuzz(input) {
-    if (typeof input !== 'number')
-        return NaN;
+  if (typeof input !== "number") return NaN;
 
-    if (input % 3 === 0 && input % 5 === 0) {
-        return 'FizzBuzz';
-    }
-    else if (input % 3 === 0) {
-        return 'Fizz';
-    }
-    else if (input % 5 === 0) {
-        return 'Buzz';
-    }
-    else
-        return input;
+  if (input % 3 === 0 && input % 5 === 0) {
+    return "FizzBuzz";
+  } else if (input % 3 === 0) {
+    return "Fizz";
+  } else if (input % 5 === 0) {
+    return "Buzz";
+  } else return input;
 }
 
 /***CHECKSPEED */
@@ -144,18 +136,23 @@ function fizzBuzz(input) {
 checkSpeed(135);
 
 function checkSpeed(speed) {
-    const SpeedLimit = 70;
-    const kmPerPoint = 5;
-    if (speed < SpeedLimit + kmPerPoint)
-        console.log('Ok');
-    else {
-        const Points = Math.floor((speed - SpeedLimit) / kmPerPoint);
+  const SpeedLimit = 70;
+  const kmPerPoint = 5;
+  if (speed < SpeedLimit + kmPerPoint) console.log("Ok");
+  else {
+    const Points = Math.floor((speed - SpeedLimit) / kmPerPoint);
 
-        if (Points > 12)
-            console.log('LICENSE SUSPENDED');
-        else
-            console.log('Points', Points);
-    }
-
+    if (Points > 12) console.log("LICENSE SUSPENDED");
+    else console.log("Points", Points);
+  }
 }
- 
+
+// document.getElementById("a").innerHTML();
+
+let nombre = "Arturo".toUpperCase();
+
+console.log(nombre);
+
+let txt = "I can eat bananas all day";
+let result = txt.slice(10, 17);
+console.log(result);
